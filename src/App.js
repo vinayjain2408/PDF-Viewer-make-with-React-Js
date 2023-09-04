459deb342b2213ad05d5d526e7d7005c3b590022
vinayjain2408 <vinay98292jain@gmail.com>
2023-09-04 12:15:06 +0530

    complete pdf

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 function App() {
   const [pdfFile, setPDFFile] = useState(null);
   const [viewpdf, setViewPdf] = useState(null);
-  const [annotations, setAnnotations] = useState([]);
+  // const [annotations, setAnnotations] = useState([]);
 
   const fileType = ['application/pdf'];
 
@@ -57,12 +57,12 @@ function App() {
               <Viewer
                 fileUrl={viewpdf}
                 plugins={[newplugin]}
-                annotations={annotations} // Pass annotations here
-                onAnnotationChange={setAnnotations} // Update annotations
+                // annotations={annotations} 
+                // onAnnotationChange={setAnnotations} 
               />
             </>
           )}
-          {!viewpdf && <>NO PDF</>}
+          {!viewpdf && <h3>NO PDF</h3>}
         </Worker>
       </div>
     </div>
@@ -70,159 +70,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
-
-// import { Viewer, Worker } from "@react-pdf-viewer/core";
-// import {  defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-// import "@react-pdf-viewer/core/lib/styles/index.css";
-// import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-
-// function App() {
-//   const [pdfFile, setPDFFile] = useState(null);
-//   const [viewpdf, setViewPdf] = useState(null);
-
-//   const fileType = ["application/pdf"];
-//   const handleChange = (e) => {
-//     let selectFile = e.target.files[0];
-//     if (selectFile) {
-//       if (selectFile && fileType.includes(selectFile.type)) {
-//         // if(selectFile && selectFile.type.includes('application/pdf')){
-//         let reader = new FileReader();
-//         reader.readAsDataURL(selectFile);
-//         reader.onload = (e) => {
-//           setPDFFile(e.target.result);
-//         };
-//       } else {
-//         setPDFFile(null);
-//       }
-//     } else {
-//       console.log("please select");
-//     }
-//   };
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (pdfFile !== null) {
-//       setViewPdf(pdfFile);
-//     } else {
-//       setViewPdf(null);
-//     }
-//   };
-
-//   const newplugin = defaultLayoutPlugin();
-
-//   return (
-//     <div className="container">
-//       <form onSubmit={handleSubmit}>
-//         <input type="file" className="form-control" onChange={handleChange} />
-//         <button type="submit" className="btn btn-success">
-//           View PDF
-//         </button>
-//       </form>
-//       <h2>View PDF</h2>
-//       <div className="pdf-container">
-//         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-//           {viewpdf && (
-//             <>
-//               <Viewer fileUrl={viewpdf} plugins={Object.values(newplugin)} />
-//             </>
-//           )}
-//           {!viewpdf && <>NO PDF</>}
-//         </Worker>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
-
-// import { Viewer, Worker } from '@react-pdf-viewer/core';
-// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-// import '@react-pdf-viewer/core/lib/styles/index.css';
-// import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-
-// function App() {
-//   const [pdfFile, setPDFFile] = useState(null);
-//   const [viewpdf, setViewPdf] = useState(null);
-
-//   const handleChange = (e) => {
-//     let selectFile = e.target.files[0];
-//     if (selectFile) {
-//       if (selectFile.type.includes('application/pdf')) {
-//         let reader = new FileReader();
-//         reader.readAsDataURL(selectFile);
-//         reader.onload = (e) => {
-//           setPDFFile(e.target.result);
-//         };
-//       } else {
-//         setPDFFile(null);
-//       }
-//     } else {
-//       console.log('please select');
-//     }
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (pdfFile !== null) {
-//       setViewPdf(pdfFile);
-//     } else {
-//       setViewPdf(null);
-//     }
-//   };
-
-//   const newplugin = defaultLayoutPlugin();
-
-//   return (
-//     <div className="container">
-//       <form onSubmit={handleSubmit}>
-//         <input type="file" className="form-control" onChange={handleChange} />
-//         <button type="submit" className="btn btn-success">
-//           View PDF
-//         </button>
-//       </form>
-//       <h2>View PDF</h2>
-//       <div className="pdf-container">
-//         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-//           {viewpdf && (
-//             <>
-//               <Viewer fileUrl={viewpdf} plugins={Object.values(newplugin)} />
-//             </>
-//           )}
-//           {!viewpdf && <>NO PDF</>}
-//         </Worker>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
